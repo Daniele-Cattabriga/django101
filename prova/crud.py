71429f9da771d38880a5c37ee789618cafe25eeb
Daniele-Cattabriga <daniele_cattabriga@libero.it>
2022-08-03 14:13:02 +0200

    Completed CRUD model, to be tested

--- prova/crud.py
+++ prova/crud.py
@@ -10,7 +10,9 @@ def get_blog_posts(user: User) -> QuerySet:
 
 
 def create_blog_post(title: str, text: str, user: User, color: str) -> BlogPost:
-    pass
+    return  BlogPost.objects.create(title=title, text=text, user=user, color=color)
+
+
 
 
 def delete_blog_post(post_id: int) -> int:
@@ -18,8 +20,17 @@ def delete_blog_post(post_id: int) -> int:
     :param post_id:
     :return: Status code -> 200 = ok, 500 = error
     """
-    pass
+    # ugly except but meh, don't know what .delete() throws
+    try:
+        BlogPost.objects.filter(id=post_id).delete()
+        return 200
+    except:
+        return 500
+
 
 
 def updated_blog_post(post_id: int, **kwargs) -> Optional[BlogPost]:
-    pass
+    BlogPost.objects.filter(id=post_id).update(**kwargs)
+    return BlogPost.objects.filter(id=post_id)
+
+#ALL IS TO BE TESTED
